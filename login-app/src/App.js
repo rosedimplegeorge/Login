f0c00f7be8f49b50326b0b7a2b72b3388175fd0d
rosedimple <rosedimplegeorge@gmail.com>
2019-04-29 11:38:12 -0400

    Login Form component created

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LoginForm from './components/LoginForm';
 
 function App() {
   return (
     <div className="App">
       <h1>Login-App</h1>
+      <LoginForm />
     </div>
   );
 }
