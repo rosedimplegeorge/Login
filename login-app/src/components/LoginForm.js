710d4bb88f6b37f3961b47b3eb5961c913cd2db4
rosedimple <rosedimplegeorge@gmail.com>
2019-04-29 11:49:48 -0400

    Form Style added-InlineStyle

--- login-app/src/components/LoginForm.js
+++ login-app/src/components/LoginForm.js
@@ -3,11 +3,25 @@ import React, { Component } from 'react';
 class LoginForm extends Component {
     render() {
         return (
-            <div>
+            <div className="login-form-container" style={formStyle}>
                 <h2>Login Form Component</h2>
+                <form></form>
             </div>
         );
     }
+}
+
+const formStyle = {
+  width: "300px",
+  border: "1px solid #d6d7da",
+  padding: "0px 15px 15px 15px",
+  borderRadius: "5px",
+  fontFamily: "arial",
+  lineHeight: "16px",
+  color: "#333333",
+  fontSize: "14px",
+  background: "#ffffff",
+  margin: "100px auto"
 }
 
 export default LoginForm;
